2de0592d93b5eb93f5b9e16f0cfdf55dc29d633b
Mirko Saugo <himynameismirko@gmail.com>
2024-01-01 21:31:53 +0100

    Refactor Card component to improve resizing functionality

--- src/Card.tsx
+++ src/Card.tsx
@@ -3,22 +3,22 @@ import { useReactFlow, NodeResizer } from "reactflow";
 import type { NodeProps } from "reactflow";
 
 const Card: React.FC<{
+  id: NodeProps["id"];
   data: NodeProps["data"];
   selected: NodeProps["selected"];
-}> = ({ data, selected }) => {
+}> = ({ data, selected, id }) => {
   const ref = React.useRef<HTMLDivElement>(null);
+
+  const flow = useReactFlow();
 
   return (
     <div
-      ref={ref}
       style={{
-        padding: 10,
         border: "1px solid #222",
         borderRadius: 5,
         background: "#fff",
         boxShadow: "0 0 5px rgba(0,0,0,0.3)",
         height: "100%",
-        width: "100%",
       }}
     >
       <NodeResizer
@@ -26,9 +26,20 @@ const Card: React.FC<{
         isVisible={selected}
         minWidth={ref.current?.offsetWidth || 0}
         minHeight={ref.current?.offsetHeight || 0}
+        onResizeEnd={(width, height) => {
+          console.log("SAVE_SIZE:", { width, height });
+        }}
       />
-      <h2>{data.label}</h2>
-      <p>{data.description}</p>
+      <div
+        ref={ref}
+        style={{
+          padding: 10,
+          width: "max-content",
+        }}
+      >
+        <h2>{data.label}</h2>
+        <p>{data.description}</p>
+      </div>
     </div>
   );
 };
